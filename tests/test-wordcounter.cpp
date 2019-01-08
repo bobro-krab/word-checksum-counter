63f0390ab84f76729353cd32a3d59cad82d88bc1
bobro-krab <bobro-krab@bk.ru>
2019-01-09 02:21:13 +0700

    Add test for whitespace separators

--- tests/test-wordcounter.cpp
+++ tests/test-wordcounter.cpp
@@ -19,3 +19,11 @@ TEST_CASE("Test unicode support", "[is unchanged]") {
     }
     REQUIRE(counter.getResult() == 3);
 }
+
+TEST_CASE("Test whitespace separators", "[separators]") {
+    std::stringstream input("mother mother\nmother\tmother\rmother");
+    WordCounter counter("mother");
+    while (input >> counter) {
+    }
+    REQUIRE(counter.getResult() == 5);
+}
